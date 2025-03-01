2754c29dc35f3efbb89f4c59cace384ab1724572
maksimangeou <ange_fuck@icloud.com>
2025-03-01 13:20:49 +0300

    HW(3)

--- src/main/java/org/skypro/sky/shop/model/service/SearchService.java
+++ src/main/java/org/skypro/sky/shop/model/service/SearchService.java
@@ -15,20 +15,10 @@ public class SearchService {
         this.storageService = new StorageService();
     }
 
-    public Collection<Searchable> search(String pattern) {
-        Collection<Searchable> collection = new ArrayList<>();
-        for(Searchable value: storageService.getAllCollection()) {
-            if(!value.searchTerm(pattern).equals(Searchable.CODE_NULL)) {
-                System.out.println(value);
-                collection.add(value);
-            }
-        }
-//        return storageService.getAllCollection().stream()
-//                .filter(i -> !i.searchTerm(pattern).equals(Searchable.CODE_NULL))
-////                .map(SearchResult::new)
-//                .collect(Collectors.toCollection(TreeSet::new));
-        return collection;
+    public Collection<SearchResult> search(String pattern) {
+        return storageService.getAllCollection().stream()
+                .filter(i -> !i.searchTerm(pattern).equals(Searchable.CODE_NULL))
+                .map(SearchResult::fromSearchable)
+                .collect(Collectors.toCollection(ArrayList::new));
     }
-
-
 }
