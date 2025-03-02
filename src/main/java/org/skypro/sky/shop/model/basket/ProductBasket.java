7e5b461862dfc33edf45cb540c84c6f79822eae0
maksimangeou <ange_fuck@icloud.com>
2025-03-02 10:08:39 +0300

    HW(1)

---
+++ src/main/java/org/skypro/sky/shop/model/basket/ProductBasket.java
@@ -0,0 +1,34 @@
+package org.skypro.sky.shop.model.basket;
+
+import org.skypro.sky.shop.model.service.StorageService;
+import org.springframework.web.context.annotation.SessionScope;
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
+
+@SessionScope
+public class ProductBasket {
+    private final Map<UUID,Integer> mapBasket;
+
+    public ProductBasket(Map<UUID, Integer> mapBasket) {
+        this.mapBasket = mapBasket;
+    }
+
+    public Map<UUID, Integer> getMapBasket() {
+        return mapBasket;
+    }
+
+    public void addProductInBasket(UUID id) {
+        StorageService storageService = new StorageService();
+        if (storageService.getProduct().containsKey(id)) {
+            if (mapBasket.containsKey(id)) {
+               mapBasket.put(id, mapBasket.get(id)+1);
+            }
+            mapBasket.put(id,1);
+        }
+    }
+
+    public Map<UUID,Integer> getProductInBasket() {
+        return Collections.unmodifiableMap(mapBasket);
+    }
+}
