0f205120036d1f1f0cf8de0dee05a34111a6f0ab
maksimangeou <ange_fuck@icloud.com>
2025-03-11 09:48:03 +0300

    HW(6)

--- src/main/java/org/skypro/sky/shop/controller/ShopControllerAdvice.java
+++ src/main/java/org/skypro/sky/shop/controller/ShopControllerAdvice.java
@@ -7,11 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.UUID;
+
 @RestControllerAdvice
 public class ShopControllerAdvice {
 
     @ExceptionHandler(NoSuchProductException.class)
-    public ResponseEntity<ShopError> catchNoSuchProductException(NoSuchProductException e) {
-        return new ResponseEntity<>(new ShopError(Integer.toString(HttpStatus.NOT_FOUND.value()), e.getMessage()), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ShopError> catchNoSuchProductException(UUID id) {
+        return new ResponseEntity<>(new ShopError(Integer.toString(HttpStatus.NOT_FOUND.value()),
+                "Продукт с id "+id+ " не найден"), HttpStatus.NOT_FOUND);
     }
 }
